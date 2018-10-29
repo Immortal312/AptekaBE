2a8a28c039843be216c2472083c82faf6b813a7b
Gabriel Hoang <timidmurder@gmail.com>
2018-10-29 19:57:10 +0100

    initial commit

---
+++ prestashop/app/config/parameters.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'parameters' => 
+  array (
+    'database_host' => '127.0.0.1',
+    'database_port' => '',
+    'database_name' => 'prestashopdb',
+    'database_user' => 'root',
+    'database_password' => '',
+    'database_prefix' => 'ps_',
+    'database_engine' => 'InnoDB',
+    'mailer_transport' => 'smtp',
+    'mailer_host' => '127.0.0.1',
+    'mailer_user' => NULL,
+    'mailer_password' => NULL,
+    'secret' => 'tQ5iJDO256u0gZQj6SaLecIz9icM0YKszELpoV9MdeVcqW9vWESR6JgC',
+    'ps_caching' => 'CacheMemcache',
+    'ps_cache_enable' => false,
+    'ps_creation_date' => '2018-10-29',
+    'locale' => 'pl-PL',
+    'cookie_key' => 'SO9jdSwFHePdeFSmJh0i27wC4iKEdRfiViRS7BOeAwMsvR12VyYK7fwG',
+    'cookie_iv' => 'R3Q2hHAB',
+    'new_cookie_key' => 'def00000bae9ab149040376785452c4298df9990fc131572b17b4b640da9d1e6ac47954bf44031735f543329f7e24a8d6d6251f985ac0bf2d201df1951d5966bbf8f0cfb',
+  ),
+);
---
+++ prestashop/var/cache/prod/smarty/compile/38/20/6c/38206c9a1a918a857bea82a2c0834b4efc360653_0.file.layout.tpl.php
@@ -0,0 +1,31 @@
+<?php
+/* Smarty version 3.1.32, created on 2018-10-29 19:48:05
+  from 'C:\xampp\htdocs\prestashop\admin637nqgacd\themes\default\template\controllers\login\layout.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.32',
+  'unifunc' => 'content_5bd755e538b356_04394074',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '38206c9a1a918a857bea82a2c0834b4efc360653' => 
+    array (
+      0 => 'C:\\xampp\\htdocs\\prestashop\\admin637nqgacd\\themes\\default\\template\\controllers\\login\\layout.tpl',
+      1 => 1537364656,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_5bd755e538b356_04394074 (Smarty_Internal_Template $_smarty_tpl) {
+echo $_smarty_tpl->tpl_vars['header']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['page']->value;?>
+
+<?php echo $_smarty_tpl->tpl_vars['footer']->value;?>
+
+<?php }
+}
